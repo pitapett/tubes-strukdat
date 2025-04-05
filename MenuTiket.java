50c87a2ac69d7d1a9217286b01baa79b7a114392
nathaliewjy <nathalie.tantiwijaya@gmail.com>
2025-04-06 01:30:55 +0700

    revisi move delete jadi switch case di main menu

--- MenuTiket.java
+++ MenuTiket.java
@@ -8,6 +8,7 @@ class User {
 //     // bisa add/remove movie, makanan, minuman di cinema terpilih;
 //     // bisa nampilin jumlah pembelian movie, makanan, minuman;
 // }
+
 class City {
 
 }
@@ -15,6 +16,7 @@ class City {
 // class Cinema {
 //     // diisi sama CafeMenu
 // }
+
 class Studio {
 
     String studioID;
@@ -70,6 +72,7 @@ class Seat {
 //     int harga;
 //     int totalPembeli;
 // }
+                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                        
 class TiketAktifNode {
 
     Ticket ticket; // ticket == node yg isinya kodeBooking dll.
@@ -92,15 +95,12 @@ class TiketAktifLinkedList {
 
         if (head == null) { // LL kosong
             head = newNode;
+            tail = head;
 
         } else { // LL udah ada isinya
-            TiketAktifNode current = head;
-
-            while (current.next != null) { // masih ada node
-                current = current.next;
-            }
-
-            current.next = newNode;
+            tail.next = newNode;
+            tail = newNode;
+            tail.next = null;
         }
     }
 
@@ -131,22 +131,26 @@ class TiketAktifLinkedList {
         }
     }
 
-    public void deleteTicket(int kodeBooking) {
+    public boolean deleteTicket(int kodeBooking) {
 
         if (head == null) { // 1st condi : LL kosong
             System.out.println("TIDAK ADA TIKET YANG DIHAPUS");
+            return false;
 
         } else if (head == tail) { // 2nd condi : node sisa 1
             if (head.ticket.kodeBooking == kodeBooking) {
                 head = null;
                 tail = null;
+                return true;
             } else {
                 System.out.println("DATA TIDAK ADA");
+                return false;
             }
 
         } else { // 3rd condi : node masih banyak
             if (head.ticket.kodeBooking == kodeBooking) { // delete head
                 head = head.next;
+                return true;
             } else {
                 TiketAktifNode current = head;
 
@@ -156,35 +160,42 @@ class TiketAktifLinkedList {
 
                 if (current.next == null) { // posisi udah jalan sampe tail tp ga nemu yg cocok
                     System.out.println("DATA TIDAK ADA");
+                    return false;
                 } else if (current.next == tail) { // posisi sblm tail & mau delete tail
                     tail = current;
                     tail.next = null;
+                    return true;
                 } else { // delete node di tengah
                     current.next = current.next.next;
+                    return true;
                 }
             }
         }
 
     }
 
-    public void moveTicket(TiketHistoryLinkedList historyList, int kodeBooking) { // kan mau move dari aktif -> history, makanya history hrs ada di parameter
+    public boolean moveTicket(TiketHistoryLinkedList historyList, int kodeBooking) { // kan mau move dari aktif -> history, makanya history hrs ada di parameter
 
         if (head == null) { // 1st condi : LL kosong
             System.out.println("TIDAK ADA TIKET YANG DIPINDAH");
+            return false;
 
         } else if (head == tail) { // 2nd condi : node sisa 1
             if (head.ticket.kodeBooking == kodeBooking) {
                 historyList.addTicket(head.ticket); // mau move si head.ticket ke historyList, jd butuh function addTicket
                 head = null;
                 tail = null;
+                return true;
             } else {
                 System.out.println("DATA TIDAK ADA");
+                return false;
             }
 
         } else { // 3rd condi : node masih banyak
             if (head.ticket.kodeBooking == kodeBooking) { // delete head
                 historyList.addTicket(head.ticket);
                 head = head.next;
+                return true;
             } else {
                 TiketAktifNode current = head;
 
@@ -194,13 +205,16 @@ class TiketAktifLinkedList {
 
                 if (current.next == null) { // posisi udah jalan sampe tail tp ga nemu yg cocok
                     System.out.println("DATA TIDAK ADA");
+                    return false;
                 } else if (current.next == tail) { // posisi sblm tail & mau delete tail
                     historyList.addTicket(current.next.ticket);
                     tail = current;
                     tail.next = null;
+                    return true;
                 } else { // delete node di tengah
                     historyList.addTicket(current.next.ticket);
                     current.next = current.next.next;
+                    return true;
                 }
             }
         }
@@ -306,10 +320,85 @@ public class MenuTiket {
 
             System.out.print("Pilih menu : ");
             pilihMenu = sc.nextInt();
+            sc.nextLine();
 
             switch (pilihMenu) {
                 case 1:
                     aktifList.showTicket();
+
+                    if (aktifList.head != null) { // kl ada tiket aktif, nanya user mau dekete / move ga
+
+                        System.out.println("Pilih : ");
+                        System.out.println("1. Move tiket");
+                        System.out.println("2. Delete tiket");
+                        System.out.println("3. Keluar program");
+
+                        System.out.print("Pilih : ");
+                        int pilih = sc.nextInt();
+                        sc.nextLine();
+
+                        switch (pilih) {
+                            case 1:
+                                System.out.print("Kode booking yang mau dipjndah : ");
+                                int kodeBookingMove = sc.nextInt();
+                                sc.nextLine();
+
+                                if (aktifList.moveTicket(historyList, kodeBookingMove)) {
+                                    System.out.println("TIKET BERHASIL DIPINDAH");
+                                } else {
+                                    System.out.println("DATA TIDAK ADA");
+                                }
+                                break;
+                            case 2:
+                                System.out.print("Kode booking yang mau dihaous : ");
+                                int kodeBookingDelete = sc.nextInt();
+                                sc.nextLine();
+
+                                if (aktifList.deleteTicket(kodeBookingDelete)) {
+                                    System.out.println("TIKET BERHASIL DIPINDAH");
+                                } else {
+                                    System.out.println("DATA TIDAK ADA");
+                                }
+                                break;
+                            default:
+                                System.out.println("Error");
+                        }
+                        // System.out.print("Move? (y/n) ");
+                        // String move = sc.next().trim();
+                        // sc.nextLine();
+
+                        // if (move.equalsIgnoreCase("y")) {
+                        //     System.out.print("Kode booking yang mau dipindah : ");
+                        //     int kodeBookingMove = sc.nextInt();
+                        //     sc.nextLine();
+
+                        //     boolean isMoved = aktifList.moveTicket(historyList, kodeBookingMove);
+
+                        //     if (isMoved) {
+                        //         System.out.println("TIKET BERHASIL DIPINDAH");
+                        //     } else {
+                        //         System.out.println("DATA TIDAK ADA");
+                        //     }
+                        // }  
+                        
+                        // System.out.print("Delete? (y/n) ");
+                        // String delete = sc.next().trim();
+
+                        // if (delete.equalsIgnoreCase("y")) {
+                        //     System.out.print("Kode booking yang mau dihaous : ");
+                        //     int kodeBookingDelete = sc.nextInt();
+                        //     sc.nextLine();
+
+                        //     boolean isDeleted = aktifList.deleteTicket(kodeBookingDelete);
+
+                        //     if (isDeleted) {
+                        //         System.out.println("TIKET BERHASIL DIHAPUS");
+                        //     } else {
+                        //         System.out.println("DATA TIDAK ADA");
+                        //     }
+                        // }
+
+                    }
                     break;
                 case 2:
                     historyList.showTicket();
