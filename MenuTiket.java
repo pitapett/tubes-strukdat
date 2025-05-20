8d0b0bfaaa27f5efb8cb22f99da4868dd7d2eb0d
Nathalie <nathalie.tantiwijaya@gmail.com>
2025-05-20 19:22:10 +0700

    gabungin code yg Peter kirim

--- MenuTiket.java
+++ MenuTiket.java
@@ -1,7 +1,18 @@
+import java.time.Duration;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
 import java.util.Scanner;
 
 class User {
 
+    TicketAktifLL ticketsAktif;
+    TicketHistoryLL ticketsHistory;
+
+    void userMenu() {
+
+    }
 }
 
 // class Admin {
@@ -11,87 +22,155 @@ class User {
 
 class City {
 
+    String cityName;
+    ArrayList<Cinema> cinemas;
+
+    public City(String cityName) {
+        this.cityName = cityName;
+    }
 }
 
-// class Cinema {
-//     // diisi sama CafeMenu
-// }
+class Cinema {
+
+    String cinemaName;
+    ArrayList<Studio> studios;
+    CafeMenu cafeMenu;
+    City city;
+
+    public Cinema(String cinemaName) {
+        this.cinemaName = cinemaName;
+    }
+}
 
 class Studio {
 
     String studioID;
-    Seat[][] seats = new Seat[3][3];
-}
+    Seat[][] seats;
+    int seatRow;
+    int seatColumn;
+    ArrayList<MovieSession> schedule;
+    Cinema cinema;
 
-class Ticket {
-
-    int kodeBooking, passKey;
-    String seatID, studioID, cinema, city, ticketID;
-
-    public Ticket(int kodeBooking, int passKey, String seatID, String studioID, String cinema, String city, String ticketID) {
-        this.kodeBooking = kodeBooking;
-        this.passKey = passKey;
-        this.seatID = seatID;
+    public Studio(String studioID, int seatRow, int seatColumn) {
         this.studioID = studioID;
-        this.cinema = cinema;
-        this.city = city;
-        this.ticketID = ticketID;
+        this.seats = new Seat[seatRow][seatColumn];
     }
-}
 
-class Movie {
+    public void initializeSeatID() {
+        for (int i = 0; i < seatRow; i++) {
+            for (int j = 0; j < seatColumn; j++) {
+                char rowChar = (char) ('A' + i); // A, B, C...
+                String colNum = String.format("%02d", j + 1); // 01, 02...99
+                seats[i][j].seatID = rowChar + colNum;
+            }
+        }
+    }
 
-    String judul, genre, ratingUsia;
-    int totalPenonton, bulanRilis;
+    public void studioLayout() {
 
-    public Movie(String judul, String genre, String ratingUsia, int totalPenonton, int bulanRilis) {
-        this.judul = judul;
-        this.genre = genre;
-        this.ratingUsia = ratingUsia;
-        this.totalPenonton = totalPenonton;
-        this.bulanRilis = bulanRilis;
+        int totalWidth = seatColumn * 7 - 1;
+        String screenHeader = "SCREEN";
+        int padding = (totalWidth - screenHeader.length()) / 2;
+
+        for (int i = 0; i < padding; i++) {
+            System.out.print(" ");
+        }
+        System.out.println(screenHeader + "\n");
+
+        for (int row = 0; row < seatRow; row++) {
+            for (int col = 0; col < seatColumn; col++) {
+                System.out.print("[" + seats[row][col].seatID + "] ");
+            }
+            System.out.println();
+        }
     }
 }
 
 class Seat {
-    
+
     String seatID;
     boolean isAvailable;
 }
 
-// class CafeMenu {
-//     // diisi makanan sama minuman
-// }
-// class Makanan {
-//     String nama;
-//     int harga;
-//     int totalPembeli;
-// }
-// class Minuman {
-    //     String nama;
-    //     int harga;
-//     int totalPembeli;
-// }
+class Movie {
 
-class TiketAktifNode {
-    
-    Ticket ticket; // ticket == node yg isinya kodeBooking dll.
-    TiketAktifNode next;
+    String judul;
+    String genre;
+    String ratingUsia;
+    int totalPenonton;
+    LocalDate bulanRilis;
+    Duration movieDuration;
 
-    public TiketAktifNode(Ticket ticket) {
-        this.ticket = ticket; // ticket masuk ke node
-        this.next = null;
+    public Movie(String judul, String genre, String ratingUsia) {
+        this.judul = judul;
+        this.genre = genre;
+        this.ratingUsia = ratingUsia;
     }
 }
 
-class TiketAktifLinkedList {
+class MovieList {
 
-    TiketAktifNode head = null;
-    TiketAktifNode tail = null;
+    ArrayList<Movie> movies;
+
+    void sortMovieByReleaseDate() {
+
+    }
+
+    void sortMovieByTotalPenonton() {
+
+    }
+
+    void printMovieList() {
+
+    }
+}
+
+class MovieSession {
+
+    Movie movie;
+    LocalDateTime startTime;
+    LocalDateTime endTime;
+    Studio studio;
+
+}
+
+class Ticket {
+
+    String ticketID;
+    int kodeBooking;
+    String passKey;
+    MovieSession session;
+    Seat seat;
+
+    public Ticket(int kodeBooking, String passKey, MovieSession session, Seat seat, String ticketID) {
+        this.kodeBooking = kodeBooking;
+        this.passKey = passKey;
+        this.session = session;
+        this.seat = seat;
+        this.ticketID = ticketID;
+    }
+
+}
+
+class TicketAktifLL {
+
+    class TicketAktifNode {
+
+        Ticket ticket; // ticket == node yg isinya kodeBooking dll.
+        TicketAktifNode next;
+
+        public TicketAktifNode(Ticket ticket) {
+            this.ticket = ticket; // ticket masuk ke node
+            this.next = null;
+        }
+    }
+
+    TicketAktifNode head = null;
+    TicketAktifNode tail = null;
 
     public void addTicket(Ticket ticket) {
 
-        TiketAktifNode newNode = new TiketAktifNode(ticket); // bikin node baru isinya ticket (wadah yg isinya kodeBooking dll.)
+        TicketAktifNode newNode = new TicketAktifNode(ticket); // bikin node baru isinya ticket (wadah yg isinya kodeBooking dll.)
 
         if (head == null) { // LL kosong
             head = newNode;
@@ -106,7 +185,7 @@ class TiketAktifLinkedList {
 
     public void showTicket() {
 
-        TiketAktifNode current = head; // krn mau show, jadi hrs mulai dari head
+        TicketAktifNode current = head; // krn mau show, jadi hrs mulai dari head
         int noTiket = 1; // entar biar hasil run nya ada urutan nomer
 
         if (current == null) {
@@ -118,14 +197,18 @@ class TiketAktifLinkedList {
                 System.out.println("TIKET KE-" + noTiket);
                 System.out.println("Kode booking : " + current.ticket.kodeBooking);
                 System.out.println("Pass key     : " + current.ticket.passKey);
-                System.out.println("Seat ID      : " + current.ticket.seatID);
-                System.out.println("Studio ID    : " + current.ticket.studioID);
-                System.out.println("Bioskop      : " + current.ticket.cinema);
-                System.out.println("Kota         : " + current.ticket.city);
+                System.out.println("Seat ID      : " + current.ticket.seat.seatID);
+                System.out.println("Studio ID    : " + current.ticket.session.studio.studioID);
+                System.out.println("Bioskop      : " + current.ticket.session.studio.cinema.cinemaName);
+                System.out.println("Kota         : " + current.ticket.session.studio.cinema.city.cityName);
                 System.out.println("Tiket ID     : " + current.ticket.ticketID);
 
+                DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd-MM-yyyy HH:mm");
+                System.out.println("Mulai        : " + current.ticket.session.startTime.format(formatter));
+                System.out.println("Selesai      : " + current.ticket.session.endTime.format(formatter));
+
                 noTiket++;
-                
+
                 current = current.next;
             }
         }
@@ -152,7 +235,7 @@ class TiketAktifLinkedList {
                 head = head.next;
                 return true;
             } else {
-                TiketAktifNode current = head;
+                TicketAktifNode current = head;
 
                 while (current.next != null && current.next.ticket.kodeBooking != kodeBooking) {
                     current = current.next; // lanjut selagi blm nemu yg cocok
@@ -174,7 +257,7 @@ class TiketAktifLinkedList {
 
     }
 
-    public boolean moveTicket(TiketHistoryLinkedList historyList, int kodeBooking) { // kan mau move dari aktif ke history, makanya history hrs ada di parameter
+    public boolean moveTicket(TicketHistoryLL ticketsHistory, int kodeBooking) { // kan mau move dari aktif ke history, makanya history hrs ada di parameter
 
         if (head == null) { // 1st condi : LL kosong
             System.out.println("TIDAK ADA TIKET YANG DIPINDAH");
@@ -182,7 +265,7 @@ class TiketAktifLinkedList {
 
         } else if (head == tail) { // 2nd condi : node sisa 1
             if (head.ticket.kodeBooking == kodeBooking) {
-                historyList.addTicket(head.ticket); // mau move si head.ticket ke historyList, jd butuh function addTicket
+                ticketsHistory.addTicket(head.ticket); // mau move si head.ticket ke ticketsHistory, jd butuh function addTicket
                 head = null;
                 tail = null;
                 return true;
@@ -193,11 +276,11 @@ class TiketAktifLinkedList {
 
         } else { // 3rd condi : node masih banyak
             if (head.ticket.kodeBooking == kodeBooking) { // delete head
-                historyList.addTicket(head.ticket);
+                ticketsHistory.addTicket(head.ticket);
                 head = head.next;
                 return true;
             } else {
-                TiketAktifNode current = head;
+                TicketAktifNode current = head;
 
                 while (current.next != null && current.next.ticket.kodeBooking != kodeBooking) {
                     current = current.next; // lanjut selagi blm nemu yg cocok 
@@ -207,12 +290,12 @@ class TiketAktifLinkedList {
                     System.out.println("DATA TIDAK ADA");
                     return false;
                 } else if (current.next == tail) { // posisi sblm tail & mau delete tail
-                    historyList.addTicket(current.next.ticket);
+                    ticketsHistory.addTicket(current.next.ticket);
                     tail = current;
                     tail.next = null;
                     return true;
                 } else { // delete node di tengah
-                    historyList.addTicket(current.next.ticket);
+                    ticketsHistory.addTicket(current.next.ticket);
                     current.next = current.next.next;
                     return true;
                 }
@@ -221,31 +304,31 @@ class TiketAktifLinkedList {
     }
 }
 
-class TiketHistoryNode {
+class TicketHistoryLL {
 
-    Ticket ticket;
-    TiketHistoryNode next;
+    class TicketHistoryNode {
 
-    public TiketHistoryNode(Ticket ticket) {
-        this.ticket = ticket;
-        this.next = null;
+        Ticket ticket;
+        TicketHistoryNode next;
+
+        public TicketHistoryNode(Ticket ticket) {
+            this.ticket = ticket;
+            this.next = null;
+        }
     }
-}
 
-class TiketHistoryLinkedList {
-
-    TiketHistoryNode head = null;
-    TiketAktifNode tail = null;
+    TicketHistoryNode head = null;
+    // TicketAktifNode tail = null;
 
     public void addTicket(Ticket ticket) {
 
-        TiketHistoryNode newNode = new TiketHistoryNode(ticket); // bikin node baru isinya ticket
+        TicketHistoryNode newNode = new TicketHistoryNode(ticket); // bikin node baru isinya ticket
 
         if (head == null) { // LL kosong
             head = newNode;
 
         } else { // LL ada isi
-            TiketHistoryNode current = head;
+            TicketHistoryNode current = head;
 
             while (current.next != null) { // masih ada node
                 current = current.next;
@@ -257,7 +340,7 @@ class TiketHistoryLinkedList {
 
     public void showTicket() {
 
-        TiketHistoryNode current = head; // mau show mulai dari head
+        TicketHistoryNode current = head; // mau show mulai dari head
         int noTiket = 1;
 
         if (current == null) {
@@ -268,11 +351,15 @@ class TiketHistoryLinkedList {
                 System.out.println("TIKET KE-" + noTiket);
                 System.out.println("Kode booking : " + current.ticket.kodeBooking);
                 System.out.println("Pass key     : " + current.ticket.passKey);
-                System.out.println("Seat ID      : " + current.ticket.seatID);
-                System.out.println("Studio ID    : " + current.ticket.studioID);
-                System.out.println("Bioskop      : " + current.ticket.cinema);
-                System.out.println("Kota         : " + current.ticket.city);
+                System.out.println("Seat ID      : " + current.ticket.seat.seatID);
+                System.out.println("Studio ID    : " + current.ticket.session.studio.studioID);
+                System.out.println("Bioskop      : " + current.ticket.session.studio.cinema.cinemaName);
+                System.out.println("Kota         : " + current.ticket.session.studio.cinema.city.cityName);
                 System.out.println("Tiket ID     : " + current.ticket.ticketID);
+
+                DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd-MM-yyyy HH:mm");
+                System.out.println("Mulai        : " + current.ticket.session.startTime.format(formatter));
+                System.out.println("Selesai      : " + current.ticket.session.endTime.format(formatter));
 
                 noTiket++;
 
@@ -285,29 +372,50 @@ class TiketHistoryLinkedList {
 public class MenuTiket {
 
     // bikin LL nya dulu
-    TiketAktifLinkedList aktifList = new TiketAktifLinkedList();
-    TiketHistoryLinkedList historyList = new TiketHistoryLinkedList();
+    TicketAktifLL ticketsAktif = new TicketAktifLL();
+    TicketHistoryLL ticketsHistory = new TicketHistoryLL();
 
     public static void main(String[] args) {
 
-        // bikin objek menu jd nnt bs akses aktifList, historyList, showMenu
+        // bikin objek menu jd nnt bs akses ticketsAktif, ticketsHistory, showMenu
         // knp bikin objek? krn drtd methodnya bukan static
         // knp gapake static? biar tiap user bs punya data yg beda2
         MenuTiket menu = new MenuTiket();
-    
-        // contoh pake data dummy
-        // urutan : kodeBooking, passKey, seatID, studioID, cinema, city, ticketID
-        Ticket ticket1 = new Ticket(1111, 1111, "A1", "S1", "CGV", "Bandung", "T1");
-        Ticket ticket2 = new Ticket(2222, 2222, "B2", "S2", "XXI", "Jakarta", "T2");
-        Ticket ticket3 = new Ticket(3333, 3333, "C3", "S3", "Cinepolis", "Surabaya", "T3");
 
-        // masukin data dummy ke aktiflist
-        menu.aktifList.addTicket(ticket1);
-        menu.aktifList.addTicket(ticket2);
-        menu.aktifList.addTicket(ticket3);
+        // contoh pake data dummy
+        // ticket1
+        City bandung = new City("Bandung");
+        Cinema cgvBandung = new Cinema("CGV");
+        cgvBandung.city = bandung;
+        bandung.cinemas = new ArrayList<>(); 
+        bandung.cinemas.add(cgvBandung);
+
+        Studio studio1 = new Studio("S1", 1, 1);
+        studio1.cinema = cgvBandung;
+        studio1.initializeSeatID();
+        cgvBandung.studios.add(studio1);
+
+        Movie dummyMovie1 = new Movie("Film 1", "Action", "PG-13");
+        MovieSession session1 = new MovieSession();
+        session1.movie = dummyMovie1;
+        session1.startTime = LocalDateTime.of(2025, 5, 19, 7, 00);
+        session1.endTime = LocalDateTime.of(2025, 5, 19, 9, 00);
+        session1.studio = studio1;
+
+        Seat seatA1 = studio1.seats[0][0];
+        if (seatA1 != null) {
+            seatA1.isAvailable = false;
+        }
+
+        Ticket ticket1 = new Ticket(1111, "P1111", session1, seatA1, "T1");
+
+        // masukin data dummy ke ticketsAktif
+        menu.ticketsAktif.addTicket(ticket1);
+        // menu.ticketsAktif.addTicket(ticket2);
+        // menu.ticketsAktif.addTicket(ticket3);
 
         menu.showMenu();
-    }    
+    }
 
     public void showMenu() {
         Scanner sc = new Scanner(System.in);
@@ -325,9 +433,9 @@ public class MenuTiket {
 
             switch (pilihMenu) {
                 case 1:
-                    aktifList.showTicket();
+                    ticketsAktif.showTicket();
 
-                    if (aktifList.head != null) { // kl ada tiket aktif, nanya user mau move / deelte ga
+                    if (ticketsAktif.head != null) { // kl ada tiket aktif, nanya user mau move / deelte ga
 
                         System.out.println("Move / delete : ");
                         System.out.println("1. Move tiket");
@@ -340,22 +448,22 @@ public class MenuTiket {
 
                         switch (pilih) {
                             case 1:
-                                System.out.print("Kode booking yang mau dipjndah : ");
+                                System.out.print("Kode booking yang mau dipindah : ");
                                 int kodeBookingMove = sc.nextInt();
                                 sc.nextLine();
 
-                                if (aktifList.moveTicket(historyList, kodeBookingMove)) {
+                                if (ticketsAktif.moveTicket(ticketsHistory, kodeBookingMove)) {
                                     System.out.println("TIKET BERHASIL DIPINDAH");
                                 } else {
                                     System.out.println("DATA TIDAK ADA");
                                 }
                                 break;
                             case 2:
-                                System.out.print("Kode booking yang mau dihaous : ");
+                                System.out.print("Kode booking yang mau dihapus : ");
                                 int kodeBookingDelete = sc.nextInt();
                                 sc.nextLine();
 
-                                if (aktifList.deleteTicket(kodeBookingDelete)) {
+                                if (ticketsAktif.deleteTicket(kodeBookingDelete)) {
                                     System.out.println("TIKET BERHASIL DIHAPUS");
                                 } else {
                                     System.out.println("DATA TIDAK ADA");
@@ -367,7 +475,7 @@ public class MenuTiket {
                     }
                     break;
                 case 2:
-                    historyList.showTicket();
+                    ticketsHistory.showTicket();
                     break;
                 case 3:
                     System.out.println("KELUAR");
@@ -376,5 +484,62 @@ public class MenuTiket {
                     System.out.println("Error");
             }
         }
+    }
+}
+
+class CafeMenu {
+
+    ArrayList<Makanan> makananList;
+    ArrayList<Minuman> minumanList;
+}
+
+class Makanan {
+
+    String nama;
+    int harga;
+    int totalPembeli;
+}
+
+class Minuman {
+
+    String nama;
+    int harga;
+    int totalPembeli;
+}
+
+// Buat Order Makanan
+class FoodOrder {
+
+    ArrayList<Makanan> orderedFood;
+    ArrayList<Minuman> orderedDrinks;
+    Ticket associatedTicket;
+    double totalPrice;
+}
+
+// Buat Admin
+class DailySalesReport {
+
+    LocalDate date;
+    int totalTicketsSold;
+    double totalFoodRevenue;
+    double totalDrinkRevenue;
+    ArrayList<Movie> topMovies;
+}
+
+// Main System Class
+class CinemaSystem {
+
+    ArrayList<City> cities;
+    ArrayList<DailySalesReport> salesReports;
+    // Empty method shells
+    // public boolean checkSeatAvailability(MovieSession session, String seatID) {
+    // }
+
+    // public DailySalesReport generateDailyReport(LocalDate date) {
+    // }
+    // public FoodOrder createFoodOrder(Ticket ticket) {
+    // }
+    public static void main(String[] args) {
+
     }
 }
